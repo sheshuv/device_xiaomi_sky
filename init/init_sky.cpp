229ad464c3b5a7540a59ce0f9d321e5bedd6d1ea
redducc <sushmit718@gmail.com>
2023-11-16 18:32:04 +0000

    sky: Add sky libinit
    Signed-off-by: redducc <sushmit718@gmail.com>

--- init/init_sky.cpp
+++ init/init_sky.cpp
@@ -1,105 +1,95 @@
 /*
-   Copyright (C) 2020 The LineageOS Project.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <vector>
-
 #include <android-base/properties.h>
+#include <sys/sysinfo.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <vector>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include "property_service.h"
+#include "vendor_init.h"
+
 using android::base::GetProperty;
 
+// list of partitions to override props
 std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
-    "vendor_dlkm.",
+    "", "odm.", "odm_dlkm.", "product.", "system.", "system_ext.", "vendor.", "vendor_dlkm.",
 };
 
-void property_override(char const prop[], char const value[], bool add = true)
-{
-    prop_info *pi;
+void property_override(char const prop[], char const value[], bool add = true) {
+    auto pi = (prop_info *)__system_property_find(prop);
 
-    pi = (prop_info *) __system_property_find(prop);
-    if (pi)
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
 
-void set_ro_build_prop(const std::string &prop, const std::string &value) {
-    for (const auto &source : ro_props_default_source_order) {
-        auto prop_name = "ro." + source + "build." + prop;
-        if (source == "")
-            property_override(prop_name.c_str(), value.c_str());
-        else
-            property_override(prop_name.c_str(), value.c_str(), false);
-    }
-};
+void set_ro_build_prop(const std::string &source, const std::string &prop,
+                       const std::string &value, bool product = false) {
+    std::string prop_name;
 
-void set_ro_product_prop(const std::string &prop, const std::string &value) {
-    for (const auto &source : ro_props_default_source_order) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
+    if (product) {
+        prop_name = "ro.product." + source + prop;
+    } else {
+        prop_name = "ro." + source + "build." + prop;
     }
-};
+
+    property_override(prop_name.c_str(), value.c_str(), true);
+}
+
+void set_device_props(const std::string fingerprint, const std::string description,
+                      const std::string brand, const std::string device, const std::string model, const std::string name, const std::string marketname) {
+    for (const auto &source : ro_props_default_source_order) {
+        set_ro_build_prop(source, "fingerprint", fingerprint);
+        set_ro_build_prop(source, "brand", brand, true);
+        set_ro_build_prop(source, "device", device, true);
+        set_ro_build_prop(source, "model", model, true);
+        set_ro_build_prop(source, "name", name, true);
+        set_ro_build_prop(source, "marketname", marketname, true);
+    }
+
+    property_override("ro.build.fingerprint", fingerprint.c_str());
+    property_override("ro.build.description", description.c_str());
+    property_override("bluetooth.device.default_name", model.c_str());
+}
 
 void vendor_load_properties() {
-    std::string region;
-    std::string sku;
-    region = GetProperty("ro.boot.hwc", "GLOBAL");
-    sku = GetProperty("ro.boot.product.hardware.sku","pro");
-
-    std::string model;
-    std::string device;
-    std::string fingerprint;
-    std::string description;
-    std::string mod_device;
-
-   
-        model = "23076RN4BI";
-        device = "sky";
-        fingerprint = "Redmi/sky/sky:12/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys";
-        description = "sky-user 12 TKQ1.221114.001 V14.0.5.0.TMWINXM release-keys";
-        //mod_device = "sweet_eea_global";
-        //property_override("ro.boot.product.hardware.sku", "sky");
-    
-
-    set_ro_build_prop("fingerprint", fingerprint);
-    set_ro_product_prop("device", device);
-    set_ro_product_prop("model", model);
-    property_override("ro.build.description", description.c_str());
-    if (mod_device != "") {
-        property_override("ro.product.mod_device", mod_device.c_str());
+    // Detect variant and override properties
+    std::string region = GetProperty("ro.boot.hwc", "");
+    std::string boardid = GetProperty("ro.boot.boardid", "");
+    if (region == "Global" && boardid == "S88029AA1") {
+	            set_device_props(                                           "Redmi/sky/sky:13/TKQ1.221114.001/V14.0.5.0.TMWEUXM:user/release-keys",
+	 "sky-user-12-TKQ1.221114.001-V14.0.5.0.TMWEUXM-release-keys", "Redmi", "rever",				 "23076RN8DY", "sky_global", "Redmi 12 5G");
+    } else if (region == "India" && boardid == "S88019EP1") {
+	set_device_props(
+	    "POCO/sky_p_in/sky:13/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys",
+            "sky-user-12-TKQ1.221114.001-V14.0.5.0.TMWINXM-release-keys", "POCO", "sky",
+            "23076PC4BI", "sky_p_in", "POCO M6 Pro 5G");
+    } else {
+        set_device_props(
+            "Redmi/sky_in/sky:13/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys",
+            "sky-user-12-TKQ1.221114.001-V14.0.5.0.TMWINXM-release-keys", "Redmi", "sky",
+            "23076RN4BI", "sky_in", "Redmi 12 5G");
     }
+    // Set hardware revision
+    property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
 }
-
